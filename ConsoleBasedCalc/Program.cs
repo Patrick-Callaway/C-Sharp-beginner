8a1dba979181c9b0bdedbdf9896d6de734288f90
Patrick <pcallaway0603@gmail.com>
2024-03-13 19:57:00 -0400

    Added basic user input questions

--- ConsoleBasedCalc/Program.cs
+++ ConsoleBasedCalc/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Basic Calculator App");
+
+        Console.WriteLine("Please enter your first number!");
+
+        var num1 = Console.ReadLine();
+
+        Console.WriteLine("Please enter your second number!");
+        var num2 = Console.ReadLine();
+    }
+}
